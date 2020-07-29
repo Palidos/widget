642992910adb110ba535715314bc670b3b27e9b3
Palidos <palidos@yandex.ru>
2020-07-29 13:39:59 +0300

    Updates

---
+++ precache-manifest.ec95942859d181620c6039dd25e280e5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d69d84f26da9278cacb93b008e27c3c6",
+    "url": "/widget/index.html"
+  },
+  {
+    "revision": "f8ccf11ac6ae6c9fbbf5",
+    "url": "/widget/static/css/2.29c4d543.chunk.css"
+  },
+  {
+    "revision": "95174111d0b6dc236a2c",
+    "url": "/widget/static/css/main.389c8322.chunk.css"
+  },
+  {
+    "revision": "f8ccf11ac6ae6c9fbbf5",
+    "url": "/widget/static/js/2.d0ca9e85.chunk.js"
+  },
+  {
+    "revision": "7a0c0758d9bfdc25a40edd58cae2b6db",
+    "url": "/widget/static/js/2.d0ca9e85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95174111d0b6dc236a2c",
+    "url": "/widget/static/js/main.6c18c698.chunk.js"
+  },
+  {
+    "revision": "1c2127e12785ded56131",
+    "url": "/widget/static/js/runtime-main.23ae560a.js"
+  },
+  {
+    "revision": "ee41f8ff5065906454ca6b2755334af9",
+    "url": "/widget/static/media/sendIcon.ee41f8ff.svg"
+  },
+  {
+    "revision": "f3f4fa3f52aa66c77d35d8a2c06483c1",
+    "url": "/widget/static/media/womanEmoji.f3f4fa3f.svg"
+  }
+]);
--- src/components/TodayPanel.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function TodayPanel() {
-  return (
-    <div className={'messages__today-text'}>{'Сегодня'}</div>
-  );
-}
